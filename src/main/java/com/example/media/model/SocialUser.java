c6f041e71aa2c3cbc53f152e6c3469bc527d7d89
M-coder-design <ghoshmayukh152@gmail.com>
2025-01-05 01:51:11 +0530

    changes

--- src/main/java/com/example/media/model/SocialUser.java
+++ src/main/java/com/example/media/model/SocialUser.java
@@ -3,7 +3,9 @@ package com.example.media.model;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 public class SocialUser {
@@ -18,4 +20,12 @@ public class SocialUser {
 
     @OneToMany(mappedBy = "socialUser")
     private List<Post> postList = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_groups",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "group_id")
+    )
+    private Set<SocialGroup> groupList = new HashSet<>();
 }
